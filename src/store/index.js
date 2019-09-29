e97670ecfe289d6bf79b0792e1a389dd2a663efb
Arslan Hayat <muhammadarslanhayat@gmail.com>
2019-09-29 23:07:23 +0500

    Add package

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 
 export const Store = React.createContext();
 const API_URL = "http://www.mocky.io/v2/5d889c8a3300002c0ed7da42";
@@ -20,13 +21,10 @@ function reducer(state, action) {
 
 export function StoreProvider(props) {
   const [state, dispatch] = React.useReducer(reducer, initialState);
-  async function fetchData() {
-    const response=await fetch(API_URL);
-    return response
-  }
   useEffect(() => {
-    const data = fetchData();
-    dispatch({ type: "LOAD_ITEMS", payload: data.items });
+    axios
+      .get(API_URL)
+      .then(data => dispatch({ type: "LOAD_ITEMS", payload: data.data.items }));
   }, []);
   const value = { state, dispatch };
   return <Store.Provider value={value}>{props.children}</Store.Provider>;
